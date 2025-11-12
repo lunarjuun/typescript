9054ae50ecb32008226483496f3dcebf43c8e8c6
Dom <157792207+lunarjuun@users.noreply.github.com>
2025-11-12 13:27:15 +0100

    fixed the test

--- test.ts
+++ test.ts
@@ -9,4 +9,6 @@ console.log(message);
 
 let fullName: string = "James";
 let age: number = 30;
-let sentence: string = "${message} my name is ${fullName} and I'm currently ${age} years old, and I will turn ${age + 1} next year."
+let sentence: string = `${message} My name is ${fullName} and I'm currently ${age} years old, and I will turn ${age + 1} next year.`;
+
+console.log(sentence);

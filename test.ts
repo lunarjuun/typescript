3a43c861255f7fdea09421ce9c610572e66b488a
Dom <157792207+lunarjuun@users.noreply.github.com>
2025-11-12 13:24:49 +0100

    testing different strings

--- test.ts
+++ test.ts
@@ -4,3 +4,9 @@ function greet(name: string): string {
 
 const message: string = greet ("there");
 console.log(message);
+
+// 
+
+let fullName: string = "James";
+let age: number = 30;
+let sentence: string = "${message} my name is ${fullName} and I'm currently ${age} years old, and I will turn ${age + 1} next year."
